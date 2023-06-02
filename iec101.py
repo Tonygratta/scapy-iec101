4ef6fe1c2260abd182c501a97412c31e5666a584
lsalab <lsalab@gmail.com>
2023-06-01 22:48:42 -0700

    All ASDU types implemented

--- iec101.py
+++ iec101.py
@@ -2,7 +2,7 @@
 
 from typing import Any, Optional
 from scapy.packet import Packet
-from scapy.fields import Field, XBitField, XByteField, XByteEnumField, XLEShortField, ByteField, BitField, BitEnumField, LEShortField, LESignedShortField, LESignedIntField, IEEEFloatField, FlagsField, PacketLenField, PacketField, XStrField, MultipleTypeField, FieldListField, PacketListField
+from scapy.fields import Field, XBitField, XByteField, XByteEnumField, XLEShortField, ByteField, BitField, BitEnumField, IEEEFloatField, LEThreeBytesField, LEShortField, LESignedShortField, LESignedIntField, FlagsField, PacketLenField, FieldLenField, StrLenField, PacketField, XStrField, MultipleTypeField, FieldListField, PacketListField
 
 FUNCTION_CODES = {
     0x0: 'SEND/CONFIRM - Reset of remote link',
@@ -224,6 +224,96 @@ RQT_ENUM = {
 RQT_ENUM.update({x: 'Reserved (compatible range)' for x in range(6, 32)})
 RQT_ENUM.update({x: 'Reserved (private)' for x in range(32, 64)})
 
+QRP_ENUM = {
+    0: 'not used',
+    1: 'general reset of process',
+    2: 'reset of pending information with time tag of the event buffer'
+}
+QRP_ENUM.update({x: 'Reserved (compatible range)' for x in range(3, 128)})
+QRP_ENUM.update({x: 'Reserved (private)' for x in range(128, 256)})
+
+FRZ_ENUM = {
+    0: 'read',
+    1: 'counter freeze without reset',
+    2: 'counter freeze with reset',
+    3: 'counter reset'
+}
+
+KPA_ENUM = {
+    0: 'not used',
+    1: 'threshold value',
+    2: 'smoothing factor',
+    3: 'low limit',
+    4: 'high limit'
+}
+KPA_ENUM.update({x: 'Reserved (compatible range)' for x in range(5, 32)})
+KPA_ENUM.update({x: 'Reserved (private)' for x in range(32, 64)})
+
+QPA_ENUM = {
+    0: 'not used',
+    1: 'previously loaded parameters',
+    2: 'parameter of the addressed object',
+    3: 'persistent cyclic or periodic transmission'
+}
+QPA_ENUM.update({x: 'Reserved (compatible range)' for x in range(4, 128)})
+QPA_ENUM.update({x: 'Reserved (private)' for x in range(128, 256)})
+
+FRQ_ENUM = {0: 'default'}
+FRQ_ENUM.update({x: 'Reserved (compatible range)' for x in range(1, 64)})
+FRQ_ENUM.update({x: 'Reserved (private)' for x in range(64, 128)})
+
+SRQ_ENUM = FRQ_ENUM
+
+SCQ_ENUM_A = {
+    0: 'default',
+    1: 'select file',
+    2: 'request file',
+    3: 'deactivate file',
+    4: 'delete file',
+    5: 'select section',
+    6: 'request section',
+    7: 'deactivate section'
+}
+SCQ_ENUM_A.update({x: 'Reserved (compatible range)' for x in range(8, 11)})
+SCQ_ENUM_A.update({x: 'Reserved (private)' for x in range(11, 16)})
+
+SCQ_ENUM_B = {
+    0: 'default',
+    1: 'requested memory space not available',
+    2: 'checksum failed',
+    3: 'unexpected communication service',
+    4: 'unexpected name of file',
+    5: 'unexpected name of section'
+}
+SCQ_ENUM_B.update({x: 'Reserved (compatible range)' for x in range(6, 11)})
+SCQ_ENUM_B.update({x: 'Reserved (private)' for x in range(11, 16)})
+
+LSQ_ENUM = {
+    0: 'not used',
+    1: 'file transfer without deactivation',
+    2: 'file transfer with deactivation',
+    3: 'section transfer without deactivation',
+    4: 'section transfer with deactivation'
+}
+LSQ_ENUM.update({x: 'Reserved (compatible range)' for x in range(5, 128)})
+LSQ_ENUM.update({x: 'Reserved (private)' for x in range(128, 256)})
+
+AFQ_ENUM_A = {
+    0: 'not used',
+    1: 'positive acknowledge of file transfer',
+    2: 'negative acknowledge of file transfer',
+    3: 'positive acknowledge of section transfer',
+    4: 'negative acknowledge of section transfer'
+}
+AFQ_ENUM_A.update({x: 'Reserved (compatible range)' for x in range(5, 11)})
+AFQ_ENUM_A.update({x: 'Reserved (private)' for x in range(11, 16)})
+
+AFQ_ENUM_B = SCQ_ENUM_B
+
+SOF_ENUM = {0: 'default'}
+SOF_ENUM.update({x: 'Reserved (compatible range)' for x in range(1, 16)})
+SOF_ENUM.update({x: 'Reserved (compatible range)' for x in range(16, 32)})
+
 CAUSE_OF_TX_FLAGS = {
     0: 'Negative',
     1: 'Test'
@@ -297,6 +387,17 @@ OCI_FLAGS = {
     3: 'CL3',
 }
 
+LPCPOP_FLAGS = {
+    0: 'LPC',
+    1: 'POP'
+}
+
+SOF_FLAGS = {
+    0: 'LFD',
+    1: 'FOR',
+    2: 'FA'
+}
+
 class NVA(Field):
     def __init__(self, name: str, default: Any, fmt: str = "<e") -> None:
         super().__init__(name, default, fmt)
@@ -337,6 +438,22 @@ class CP56Time2a(Packet):
 
     def extract_padding(self, s: bytes):
         return b'', s
+
+class SOF(Packet):
+    name = 'Status of file'
+    fields_desc = [
+        FlagsField('flags', 0b000, 3, SOF_FLAGS),
+        BitEnumField('status', 0b00000, 5, SOF_ENUM)
+    ]
+
+class IOFile(Packet):
+    name = 'entry'
+    fields_desc = [
+        LEShortField('NOF', 0x0000),
+        LEThreeBytesField('LOF', 0x000000),
+        PacketField('SOF', SOF(), SOF),
+        PacketField('created', CP56Time2a(), CP56Time2a)
+    ]
 
 class IOVal(Packet):
     name = 'Information object value'
@@ -415,6 +532,48 @@ class VTI(IOVal):
         BitField('value', 0b0000000, 7),
     ]
 
+class QCC(IOVal):
+    name = 'Qualifier of counter interrogation command'
+    fields_desc = [
+        BitEnumField('FRZ', 0b00, 2, FRZ_ENUM),
+        BitEnumField('RQT', 0b000000, 6, RQT_ENUM)
+    ]
+
+class QPM(IOVal):
+    name = 'Qualifier of parameter of measured values'
+    fields_desc = [
+        FlagsField('parameter', 0b00, 2, LPCPOP_FLAGS),
+        BitEnumField('KPA', 0b000000, 6, KPA_ENUM)
+    ]
+
+class FRQ(IOVal):
+    name = 'File ready qualifier'
+    fields_desc = [
+        BitField('PN', 0b0, 1),
+        BitEnumField('qualifier', 0b0000000, 7, FRQ_ENUM)
+    ]
+
+class SRQ(IOVal):
+    name = 'Section ready qualifier'
+    fields_desc = [
+        BitField('ready', 0b0, 1),
+        BitEnumField('qualifier', 0b000000, 7, SRQ_ENUM)
+    ]
+
+class SCQ(IOVal):
+    name = 'Select and call qualifier'
+    fields_desc = [
+        BitEnumField('error', 0x0, 4, SCQ_ENUM_B),
+        BitEnumField('qualifier', 0x0, 4, SCQ_ENUM_A)
+    ]
+
+class AFQ(IOVal):
+    name = 'Acknowledge file or section qualifier'
+    fields_desc = [
+        BitEnumField('error', 0x0, 4, AFQ_ENUM_B),
+        BitEnumField('qualifier', 0x0, 4, AFQ_ENUM_A)
+    ]
+
 class IO(Packet):
     name = 'Information object'
     __slots__ = ['sq', 'number']
@@ -433,7 +592,7 @@ class IO1(IO):
         XLEShortField('IOA', 0x0000),
         MultipleTypeField(
             [
-                (FieldListField('SIQ', [], FlagsField('', 0x00, 8, SIQ_FLAGS), count_from=lambda pkt: pkt.number), lambda pkt: pkt.sq == 1),
+                (FieldListField('SIQ', [], FlagsField('', 0x00, 8, SIQ_FLAGS), length_from=lambda pkt: pkt.number), lambda pkt: pkt.sq == 1),
             ],
             FlagsField('SIQ', 0x00, 8, SIQ_FLAGS)
         )
@@ -821,8 +980,144 @@ class IO70(IO):
 class IO100(IO):
     name = 'Interrogation command'
     fields_desc = [
-        XByteField('IOA', 0x0000),
+        XLEShortField('IOA', 0x0000),
         XByteEnumField('QOI', 0x0000, QOI_ENUM),
+    ]
+
+class IO101(IO):
+    name = 'Counter interrogation command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        PacketField('QCC', QCC(), QCC)
+    ]
+
+class IO102(IO):
+    name  = 'Read command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000)
+    ]
+
+class IO103(IO):
+    name = 'Clock synchronization command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        PacketField('time', CP56Time2a(), CP56Time2a)
+    ]
+
+class IO104(IO):
+    name = 'Test command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        XLEShortField('FBP', 0x55aa)
+    ]
+
+class IO105(IO):
+    name = 'Reset process command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        XByteEnumField('QRP', 0x00, QRP_ENUM)
+    ]
+
+class IO106(IO):
+    name = 'Delay acquisition command'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('delay_ms', 0x0000)
+    ]
+
+class IO110(IO):
+    name = 'Parameter of measured values, normalized value'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        NVA('NVA', 0.0),
+        PacketField('QPM', QPM(), QPM)
+    ]
+
+class IO111(IO):
+    name = 'Parameter of measured values, scaled value'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LESignedShortField('SVA', 0x0000),
+        PacketField('QPM', QPM(), QPM)
+    ]
+
+class IO112(IO):
+    name = 'Parameter of measured values, short floating point number'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        IEEEFloatField('value', 0.0),
+        PacketField('QPM', QPM(), QPM)
+    ]
+
+class IO113(IO):
+    name = 'Parameter activation'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        XByteEnumField('QPA', 0x00, QPA_ENUM)
+    ]
+
+class IO120(IO):
+    name = 'File ready'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        LEThreeBytesField('LOF', 0x000000),
+        PacketField('FRQ', FRQ(), FRQ)
+    ]
+
+class IO121(IO):
+    name = 'Section ready'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        ByteField('NOS', 0x00),
+        LEThreeBytesField('LOF', 0x000000),
+        PacketField('SRQ', SRQ(), SRQ)
+    ]
+
+class IO122(IO):
+    name = 'Call directory, select file, call file, call section'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        ByteField('NOS', 0x00),
+        PacketField('SCQ', SCQ(), SCQ)
+    ]
+
+class IO123(IO):
+    name = 'Last section, last segment'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        ByteField('NOS', 0x00),
+        XByteEnumField('LSQ', 0x00, LSQ_ENUM),
+        XByteField('CHS', 0x00)
+    ]
+
+class IO124(IO):
+    name = 'ACK file, ACK section'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        ByteField('NOS', 0x00),
+        PacketField('AFQ', AFQ(), AFQ)
+    ]
+
+class IO125(IO):
+    name = 'Segment'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        LEShortField('NOF', 0x0000),
+        ByteField('NOS', 0x00),
+        FieldLenField('LOS', 0x00, length_of='segment', fmt='B'),
+        StrLenField('segment', b'', length_from=lambda pkt: pkt.LOS, max_length=255)
+    ]
+
+class IO126(IO):
+    name = 'Directory'
+    fields_desc = [
+        XLEShortField('IOA', 0x0000),
+        PacketListField('entries', [], IOFile, length_from=lambda pkt: pkt.number)
     ]
 
 class ASDU(Packet):
@@ -886,6 +1181,23 @@ class ASDU(Packet):
                 (PacketField('IO', IO51(), IO51), lambda pkt: pkt.type == 0x33),
                 (PacketField('IO', IO70(), IO70), lambda pkt: pkt.type == 0x46),
                 (PacketField('IO', IO100(), IO100), lambda pkt: pkt.type == 0x64),
+                (PacketField('IO', IO101(), IO101), lambda pkt: pkt.type == 0x65),
+                (PacketField('IO', IO102(), IO102), lambda pkt: pkt.type == 0x66),
+                (PacketField('IO', IO103(), IO103), lambda pkt: pkt.type == 0x67),
+                (PacketField('IO', IO104(), IO104), lambda pkt: pkt.type == 0x68),
+                (PacketField('IO', IO105(), IO105), lambda pkt: pkt.type == 0x69),
+                (PacketField('IO', IO106(), IO106), lambda pkt: pkt.type == 0x6a),
+                (PacketField('IO', IO110(), IO110), lambda pkt: pkt.type == 0x6e),
+                (PacketField('IO', IO111(), IO111), lambda pkt: pkt.type == 0x6f),
+                (PacketField('IO', IO112(), IO112), lambda pkt: pkt.type == 0x70),
+                (PacketField('IO', IO113(), IO113), lambda pkt: pkt.type == 0x71),
+                (PacketField('IO', IO120(), IO120), lambda pkt: pkt.type == 0x78),
+                (PacketField('IO', IO121(), IO121), lambda pkt: pkt.type == 0x79),
+                (PacketField('IO', IO122(), IO122), lambda pkt: pkt.type == 0x7a),
+                (PacketField('IO', IO123(), IO123), lambda pkt: pkt.type == 0x7b),
+                (PacketField('IO', IO124(), IO124), lambda pkt: pkt.type == 0x7c),
+                (PacketField('IO', IO125(), IO125), lambda pkt: pkt.type == 0x7d),
+                (PacketField('IO', IO126(), IO126), lambda pkt: pkt.type == 0x7e),
             ],
             XStrField('IO', b'')
         ),
